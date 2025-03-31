a8336f7176330ebab7f3db9e9fbe568ee7ee2279
Nicolas Isnardi <nicolasisnardi@gmail.com>
2025-03-31 19:23:22 -0400

    * removed credentials

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAMDMIf_Ea7ve8qaP9EePkDTtV6e591oYE",
-  authDomain: "authentication-40a73.firebaseapp.com",
-  projectId: "authentication-40a73",
-  storageBucket: "authentication-40a73.firebasestorage.app",
-  messagingSenderId: "195198547116",
-  appId: "1:195198547116:web:15284f3efff55a470adb09",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
